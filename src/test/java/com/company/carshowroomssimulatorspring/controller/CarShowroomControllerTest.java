84871bf2c85007559a29e762dc3fffa28bd86f4c
PiotrIzw <piotr.izworski.0@gmail.com>
2020-12-28 16:20:30 +0100

    Added unit tests to controller classes.

---
+++ src/test/java/com/company/carshowroomssimulatorspring/controller/CarShowroomControllerTest.java
@@ -0,0 +1,34 @@
+package com.company.carshowroomssimulatorspring.controller;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.io.IOException;
+//TODO
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class CarShowroomControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    protected String mapToJson(Object obj) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.writeValueAsString(obj);
+    }
+    protected <T> T mapFromJson(String json, Class<T> clazz)
+            throws JsonParseException, JsonMappingException, IOException {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(json, clazz);
+    }
+}
--- src/test/java/com/company/carshowroomssimulatorspring/controller/RatingControllerTest.java
+++ src/test/java/com/company/carshowroomssimulatorspring/controller/RatingControllerTest.java
@@ -1,19 +1,15 @@
 package com.company.carshowroomssimulatorspring.controller;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import com.company.carshowroomssimulatorspring.model.ItemCondition;
+import com.company.carshowroomssimulatorspring.model.Rating;
+import com.company.carshowroomssimulatorspring.model.RatingEnum;
 import com.company.carshowroomssimulatorspring.model.Vehicle;
+import com.company.carshowroomssimulatorspring.repository.CarShowroomRepository;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -23,12 +19,12 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.io.IOException;
-import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class VehicleControllerTest {
+class RatingControllerTest {
 
     @Autowired
     private MockMvc mvc;
@@ -45,9 +41,8 @@ public class VehicleControllerTest {
     }
 
     @Test
-    public void shouldGetValidResponseCodeWhenGettingAllCars() throws Exception {
-
-        String uri = "/api/product";
+    public void shouldReturnValidResponseCodeWhenGettingAllReviews() throws Exception {
+        String uri = "/api/rating";
         MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
                 .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
 
@@ -55,41 +50,22 @@ public class VehicleControllerTest {
         assertEquals(200, status);
 
         String content = mvcResult.getResponse().getContentAsString();
-        Vehicle[] vehicleList = mapFromJson(content, Vehicle[].class);
+        Rating[] vehicleList = mapFromJson(content, Rating[].class);
         assertTrue(vehicleList.length > 0);
     }
 
     @Test
-    public void shouldGetValidResponseCodeWhenAddingNewCar() throws Exception {
+    public void shouldReturnValidResponseCodeWhenAddingAReview() throws Exception {
+        String uri = "/api/rating";
+        Rating rating = new Rating();
+        rating.setRating(RatingEnum.Five);
+        rating.setShowroomId(5);
 
-            String uri = "/api/product";
-            Vehicle car = new Vehicle("Fiat", "Uno", ItemCondition.NEW, 40000.0, 2020, 180, 1.6, 1, false);
-            car.setShowroomId(5);
-            String inputJson = mapToJson(car);
-            MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post(uri)
-                    .contentType(MediaType.APPLICATION_JSON_VALUE).content(inputJson)).andReturn();
-
-            int status = mvcResult.getResponse().getStatus();
-            assertEquals(200, status);
-        }
-
-    @Test
-    public void shouldGetValidResponseCodeWhenDeletingACar() throws Exception {
-        String uri = "/api/product/6";
-        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.delete(uri)).andReturn();
-        int status = mvcResult.getResponse().getStatus();
-        assertEquals(200, status);
-    }
-
-    @Test
-    public void shouldGetValidResponseCodeWhenGettingAllCarsCSVList() throws Exception {
-        String uri = "/api/product/csv";
-        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
-                .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+        String inputJson = mapToJson(rating);
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post(uri)
+                .contentType(MediaType.APPLICATION_JSON_VALUE).content(inputJson)).andReturn();
 
         int status = mvcResult.getResponse().getStatus();
         assertEquals(200, status);
-
     }
-
 }
